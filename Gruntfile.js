57fc040c31ec59ae5aa38ac3763466685db5178c
Martin Volerich <martin@volerich.com>
2014-05-19 16:01:17 -0400

    Updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,5 +19,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
+  grunt.registerTask('default', ['jshint', 'karma']);
 
 };
