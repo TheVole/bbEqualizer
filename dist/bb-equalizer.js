1a9f7f664fd21a3498663b897dbb121968502499
Martin Volerich <martin@volerich.com>
2014-07-02 09:23:55 -0400

    Rework logic

--- dist/bb-equalizer.js
+++ dist/bb-equalizer.js
@@ -8,100 +8,86 @@
 
           transclude: true,
 
-          scope: {},
-
           template: '<div ng-transclude></div>',
 
-          controller: ['$scope', function ($scope) {
-            var window = angular.element($window);
+          scope: {
+            name: '@'
+          },
 
-            var partInfos = [];
+          controller: function ($scope) {
+            var controlledParts = Object.create(null);
 
-            $scope.$on('$routeChangeSuccess', function () {
-              _.remove(partInfos, function (partID) {
-                return !partID.keep;
+            parts = function () {
+              return _.values(controlledParts);
+            };
+
+            var w = angular.element($window);
+            w.bind('resize', recalc);
+
+            this.addPart = function (name, part) {
+              controlledParts[name] = part;
+            };
+
+            function recalc() {
+              angular.forEach(parts(), function (partElem) {
+                partElem.css('height', 'auto');
+                partElem.css({'transition': 'height 0s', 'height': ''});
               });
+              var height = calculateHeight(parts());
+              setAllPartsToHeight(height);
+              console.log("Parts count:", parts().length);
+            }
+
+            this.recalc = function () {
+              recalc();
+            };
+
+            $scope.$watch('controlledParts', function (newValue, oldValue) {
+              var height = calculateHeight(newValue);
+              setAllPartsToHeight(height);
             });
 
-            var partFromID = function (id) {
-              return angular.element('[bb-equalizer-part="' + id + '"]');
-            };
-
-            this.addPart = function (attr, keep) {
-              partInfos.push({id: attr, keep: keep});
-            };
-
-            this.removePart = function (attr) {
-              _.remove(partInfos, function (aPart) {
-                return aPart.id === attr;
+            function calculateHeight(newValue) {
+              var parts = _.values(newValue);
+              var tallest = _(parts).map(function (partElement) {
+                return partElement.height();
               });
-            };
+              var max = tallest.max().value();
+              return max;
+            }
 
-            var saveOldHeights = function () {
-              var oldHeights = [];
-              angular.forEach(partInfos, function (partInfo) {
-                var part = partFromID(partInfo.id);
-                oldHeights.push(part.height());
-                part.css({'transition': 'height 0s', 'height': ''});
+            function setAllPartsToHeight(height) {
+              angular.forEach(parts(), function (partElem) {
+                partElem.height(height + 'px');
               });
-              return oldHeights;
-            };
+            }
 
-            var restoreOldHeights = function (oldHeights) {
-              for (var i = 0; i < oldHeights.length; i++) {
-                var partID = partInfos[i].id;
-                var part = partFromID(partID);
-                part.height(oldHeights[i]);
-              }
-            };
-
-            var getMaxHeight = function () {
-              var oldHeights = saveOldHeights();
-
-              var maxPartID = '';
-              var maxHeight = 0;
-
-              maxPartID = _.max(partInfos, function (partInfo) {
-                return partFromID(partInfo.id).height();
-              });
-              maxHeight = partFromID(maxPartID.id).height();
-
-              restoreOldHeights(oldHeights);
-
-              return { max: maxHeight, numItems: partInfos.length };
-            };
-
-            var setPartsHeight = function (height) {
-              angular.forEach(partInfos, function (partInfo) {
-                var part = partFromID(partInfo.id);
-                part.height(height);
-              });
-            };
-
-            $scope.$watch(getMaxHeight, function (newValue, oldValue) {
-              setPartsHeight(newValue.max);
-            }, true);
-
-            window.on('resize', function () {
-              $scope.$apply();
-            });
           }
-          ]
         };
+
       }])
 
-    .directive('bbEqualizerPart', function () {
-      return {
-        restrict: 'A',
-        require: '^bbEqualizer',
-        transclude: true,
-        template: '<div ng-transclude></div>',
-        link: function (scope, element, attrs, equalizerCtrl) {
-          var attr = attrs.bbEqualizerPart;
-          var keep = attrs.keep;
-          equalizerCtrl.removePart(attr);
-          equalizerCtrl.addPart(attr, keep);
-        }
-      };
-    });
+    .directive('bbEqualizerPart', ['$timeout',
+      function ($timeout) {
+        return {
+          restrict: 'A',
+          require: '^bbEqualizer',
+          scope: {
+            name: '@',
+            refreshOn: '=bbRefreshOn'
+          },
+          transclude: true,
+          replace: true,
+          template: '<div ng-transclude></div>',
+          link: function (scope, element, attrs, equalizer) {
+            equalizer.addPart(attrs.name, element);
+            scope.$watchCollection('refreshOn', function () {
+              $timeout(
+                equalizer.recalc,
+                10
+              );
+            });
+          }
+        };
+      }]);
 })();
